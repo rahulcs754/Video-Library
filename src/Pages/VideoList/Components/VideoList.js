1d03ace05c38619346a3fc7ac69d952b47b063d3
rahulcs754 <rahul.shukla754@gmail.com>
2022-03-27 17:16:16 +0530

    Added text into menu bar

--- src/Pages/VideoList/Components/VideoList.js
+++ src/Pages/VideoList/Components/VideoList.js
@@ -5,37 +5,39 @@ export const VideoList = () => {
   const { data: ListOFVideo } = VideoState;
 
   return (
-    <div className="col-10 col-md-10 col-lg-10 ml-l mt-l">
-      <div className="flex flex-row flex-center gap-sm product_list_container">
-        {ListOFVideo &&
-          ListOFVideo.map((item, i) => {
-            const { _id, title, likes, views, thumnailMedium } = item;
+    <>
+      <div className="col-10 col-md-10 col-lg-10 ml-l mt-l">
+        <div className="flex flex-row flex-center gap-sm product_list_container">
+          {ListOFVideo &&
+            ListOFVideo.map((item, i) => {
+              const { _id, title, likes, views, thumnailMedium } = item;
 
-            const titleSet =
-              title.length > 30 ? title.slice(0, 30) + "..." : title;
+              const titleSet =
+                title.length > 30 ? title.slice(0, 30) + "..." : title;
 
-            return (
-              <div className="card card-overlay" key={i}>
-                <img
-                  src={thumnailMedium.url}
-                  className="card-image img-c"
-                  alt="Card-Image"
-                />
+              return (
+                <div className="card card-overlay" key={i}>
+                  <img
+                    src={thumnailMedium.url}
+                    className="card-image img-c"
+                    alt="Card-Image"
+                  />
 
-                <div className="card-header">
-                  <div className="card-title video_title">{titleSet}</div>
+                  <div className="card-header">
+                    <div className="card-title video_title">{titleSet}</div>
+                  </div>
+                  <div className="card-info">
+                    <p>{views} views</p>
+                    <p>{likes} likes</p>
+                  </div>
+                  <div className="card-footer ">
+                    <VideoCartFooter {...item} />
+                  </div>
                 </div>
-                <div className="card-info">
-                  <p>{views} views</p>
-                  <p>{likes} likes</p>
-                </div>
-                <div className="card-footer ">
-                  <VideoCartFooter {...item} />
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
